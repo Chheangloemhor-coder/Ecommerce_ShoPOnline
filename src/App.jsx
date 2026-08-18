cf214a4cd08e1c7e8c6323ac7ef6df98fd4f09d0
Chheangloemhor-coder <chheangloemhor130608@gmail.com>
2026-08-18 19:11:02 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Mainlayout/>}>
-      <Route path='/Home' element={<HomePage/>}/>
+      <Route path='/' element={<HomePage/>}/>
       <Route path="/shop" element={<Shoppage/>}/>
       <Route path='/product/:id' element={<ProductDetail/>}/>
       </Route>
